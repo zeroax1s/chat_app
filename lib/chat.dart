3c70d20729d303ddd1d033598c5fe888c569eda6
DHM <iron.steel135@gmail.com>
2023-06-08 21:15:06 -0500

    added send chat functionality

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: camel_case_types
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,6 +21,18 @@ class MyChat extends StatelessWidget {
   }
 }
 
+class Chat {
+  final String content;
+  final String sender;
+  final DateTime timestamp;
+
+  Chat({
+    required this.content,
+    required this.sender,
+    required this.timestamp,
+  });
+}
+
 class chatPage extends StatefulWidget {
   const chatPage({super.key});
 
@@ -27,6 +41,22 @@ class chatPage extends StatefulWidget {
 }
 
 class _chatPageState extends State<chatPage> {
+  List<Chat> chats = [];
+  String userInput = '';
+  void sendChat() {
+    if (userInput.isNotEmpty) {
+      Chat newChat = Chat(
+        content: userInput,
+        sender: 'zeroax1s',
+        timestamp: DateTime.now(),
+      );
+      setState(() {
+        chats.add(newChat);
+        userInput = ''; // Clear the input field after sending the message
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,19 +64,44 @@ class _chatPageState extends State<chatPage> {
         centerTitle: true,
         title: Text("Pull title from main page"),
       ),
-      body: Center(
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: TextFormField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Type Your Message',
-              suffixIcon: IconButton(
-                color: Colors.lightGreen,
-                onPressed: () {},
-                icon: Icon(Icons.send),
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: chats.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Chat message = chats[index];
+                    return ListTile(
+                      title: Text(message.content),
+                      subtitle: Text(message.sender),
+                      trailing: Text(message.timestamp.toString()),
+                    );
+                  },
+                ),
               ),
-            ),
+              TextFormField(
+                onChanged: (value) {
+                  setState(() {
+                    userInput = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.lightGreen),
+                  ),
+                  labelText: 'Type Your Message',
+                  suffixIcon: IconButton(
+                    color: Colors.lightGreen,
+                    onPressed: sendChat,
+                    icon: Icon(Icons.send),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
